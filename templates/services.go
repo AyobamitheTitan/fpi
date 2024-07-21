c725507693cb72b6e5335b55164670b1c9518451
Oluwatimilehin <ayobamijijnr@gmail.com>
2024-07-21 23:40:41 +0100

    Add template to write to generated service file

---
+++ templates/services.go
@@ -0,0 +1,36 @@
+package templates
+
+var serviceTemplate string
+
+func ServiceTemplate() string{
+
+	serviceTemplate = `
+class %sService:
+	def __init__(self):
+		pass
+
+	
+	async def get(self,):
+		pass
+
+
+	async def get_by_id(self, ID):
+		pass
+
+
+	async def update(self, ID):
+		pass
+
+
+	
+	async def store(self, body):
+		pass
+
+
+	
+	async def delete(self, ID):
+		pass
+`
+
+	return serviceTemplate
+}
