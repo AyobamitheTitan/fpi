939a7abeaf2c15adfe767c5a9405eb0958f698bd
Oluwatimilehin <ayobamijijnr@gmail.com>
2024-09-18 20:08:40 +0100

    make the class names PascalCase

--- create/write.go
+++ create/write.go
@@ -7,14 +7,25 @@ import (
 	"fpi/templates"
 )
 
+func MakePascalCase(word string) string{
+	splitWord := strings.Split(word,"_")
+	pascalCase := ""
+
+	for _,word := range(splitWord){
+		pascalCase += strings.Title(word)
+	}
+	return pascalCase
+}
+
 func WriteToFile(dirName string, fileName string) error{
 	fileCreate := fileName + ".py"
+	pascalCase := MakePascalCase(fileName)
 
 	if dirName == "routers"{
 		defaultRouter := fmt.Sprintf(
 			templates.RouterTemplate(),
 			fileName,
-			strings.Title(fileName),
+			pascalCase,
 			fileName,
 			fileName,
 			fileName,
@@ -26,29 +37,29 @@ func WriteToFile(dirName string, fileName string) error{
 	} else if dirName == "controllers"{
 		defaultController := fmt.Sprintf(
 			templates.ControllerTemplate(),
-			strings.Title(fileName),
+			pascalCase,
 		)
 		return os.WriteFile(fileCreate, []byte(defaultController), os.FileMode(0755))
 
 	} else if dirName == "models"{
 		defaultModel := fmt.Sprintf(
 			templates.ModelTemplate(),
-			strings.Title(fileName),
+			pascalCase,
 		)
 		return os.WriteFile(fileCreate, []byte(defaultModel), os.FileMode(0755))
 
 	} else if dirName == "schemas"{
 		defaultSchema := fmt.Sprintf(
 			templates.SchemaTemplate(),
-			strings.Title(fileName),
-			strings.Title(fileName),
+			pascalCase,
+			pascalCase,
 		)
 		return os.WriteFile(fileCreate, []byte(defaultSchema), os.FileMode(0755))
 
 	} else if dirName == "services"{
 		defaultService := fmt.Sprintf(
 			templates.ServiceTemplate(),
-			strings.Title(fileName),
+			pascalCase,
 		)
 		return os.WriteFile(fileCreate, []byte(defaultService), os.FileMode(0755))
 		
